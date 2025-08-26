3c33157e993bc400b13f7db504449a9553b07f8c
Mahasivam <mahasivam.it@gmail.com>
2025-08-27 02:03:55 +0530

    ocpp

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
+import ChargingStations from './pages/ChargingStations';
+import ChargingStationDetail from './pages/ChargingStationDetail';
+import Transactions from './pages/Transactions';
+import IdTags from './pages/IdTags';
 
 function App() {
   return (
@@ -9,7 +13,10 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          {/* Add other routes here */}
+          <Route path="/charging-stations" element={<ChargingStations />} />
+          <Route path="/charging-stations/:id" element={<ChargingStationDetail />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/id-tags" element={<IdTags />} />
         </Routes>
       </Layout>
     </Router>
